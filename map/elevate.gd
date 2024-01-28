5c45f2180a70d4f5fa676c4e975bb1e3e9a582d2
Isaac Lima <isaac.lima1@outlook.com>
2024-01-28 15:17:44 -0300

    almost finished the level 1

--- map/elevate.gd
+++ map/elevate.gd
@@ -2,13 +2,9 @@ extends StaticBody3D
 
 @export var activators : Array[Node]
 @export var time : float = 1
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	var total_act = len(activators)
 	var act_n = 0
 	for act in activators:
@@ -18,4 +14,3 @@ func _process(delta):
 	var tween = create_tween()
 	tween.set_ease(Tween.EASE_OUT)
 	tween.tween_property($CollisionShape3D, 'position', $End.position if act_n == total_act else $Start.position, time)
-	pass
