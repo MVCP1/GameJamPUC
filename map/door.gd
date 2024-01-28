afec198c5527721eec8bc0e6c1fc1acf39323891
Marcos Pacheco <vinipacheco08@hotmail.com>
2024-01-28 15:17:11 -0300

    Door and more

---
+++ map/door.gd
@@ -0,0 +1,31 @@
+extends Area3D
+
+@export var activators : Array[Node]
+
+var open = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$Closed.visible = not open
+	$Open.visible = open
+	
+	var total_act = len(activators)
+	var act_n = 0
+	for act in activators:
+		if act.active:
+			act_n += 1
+	if(act_n == total_act):
+		open = true
+	print(open, $Open.visible, $Closed.visible)
+	pass
+
+
+func _on_body_entered(body):
+	if body == Game.get_player() and open:
+		Game.next_level()
+	pass # Replace with function body.
