d572575852981956df62e2aa80645c820a7cfda0
Marcos Pacheco <vinipacheco08@hotmail.com>
2024-01-28 16:06:27 -0300

    Ui

---
+++ UI/Scripts/Barra de Luz.gd
@@ -0,0 +1,14 @@
+extends TextureProgressBar
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		value -= 1
+	elif Input.is_action_pressed("ui_down"):
+		value += 1
--- utility/game.gd
+++ utility/game.gd
@@ -1,13 +1,18 @@
 extends Node
 
-var current_level = 1
+var current_level = 0
+var volume = 0.0
 #const levels = [
 	#
 #]
 
+func _process(_delta):
+	if Input.is_action_pressed("esc"):
+		get_tree().quit()
+
 func next_level():
 	current_level += 1
-	get_tree().change_scene("res://scene/level"+str(current_level)+".tscn")
+	get_tree().change_scene_to_file("res://scene/level"+str(current_level)+".tscn")
 	
 func die():
 	get_tree().reload_current_scene()
